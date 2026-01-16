796037f358e54492997aa4448988acb9867f1ee3
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-16 23:41:12 +0000

    5th task in the 4th project import modules

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-if __name__ == "__main__":
-    names = dir(hidden_4)
-    names.sort()
-    for name in names:
-        if not name.startswwith("__"):
-            print(name)
