1ad1202ecc6fe98b24f6542b262b4f986591bea9
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-18 15:07:39 +0300

    5th task in the Python - Data Structures: Lists, Tuples

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    list_copy = my_list[:]
     if idx < 0:
-        return my_list.copy()
+        return list_copy
     if idx >= len(my_list):
-        return my_list.copy()
+        return list_copy
