2ff205bb62cc79c766f6c8af31aad5e796ea81e3
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-22 10:21:32 +0300

    2nd advanced task in Python -Test-driven development project

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,7 +10,7 @@ def lazy_matrix_mul(m_a, m_b):
     Multiplies two matrices using NumPy
     """
     try:
-        return np.matrix(m_a) * np.matrix(m_b)
+        return np.matmul(m_a, m_b)
     except Exception:
         if isinstance(m_a, str) or isinstance(m_b, str):
             raise TypeError("Scalar operands are not allowed, use '*' instead")
