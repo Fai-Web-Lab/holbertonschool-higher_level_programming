77913adbfb99dc6662390a9fc29c6f39893ef6a5
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-22 09:57:25 +0300

    2nd advanced task in Python -Test-driven development project

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""
-This module provides a function for matrix multiplication using NumPy.
-It leverages NumPy's optimized C backend for faster calculations.
-"""
+"""Module for lazy matrix multiplication using NumPy"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices using the NumPy module.
-
-    Args:
-        m_a: First matrix (list of lists).
-        m_b: Second matrix (list of lists).
-
-    Returns:
-        The result of the multiplication.
-    """
+    """Multiplies two matrices using np.matmul"""
     return np.matmul(m_a, m_b)
