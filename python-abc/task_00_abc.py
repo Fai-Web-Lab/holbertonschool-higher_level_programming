5bdd350a59c0c33339c7c5e864ad109a14280eb7
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-30 15:57:12 +0300

    1st task in Python - Abstract Classes and Interfaces project

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines an abstract base class Animal and its subclasses.
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract Base Class representing an Animal.
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method that must be implemented by subclasses.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Subclass of Animal representing a Dog.
+    """
+
+    def sound(self):
+        """
+        Returns the sound of a dog.
+        """
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Subclass of Animal representing a Cat.
+    """
+
+    def sound(self):
+        """
+        Returns the sound of a cat.
+        """
+        return "Meow"
