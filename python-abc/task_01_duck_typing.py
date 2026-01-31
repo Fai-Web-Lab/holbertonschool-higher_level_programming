a62c3f4099b32c1b00b6923f1291b123e65fbcc7
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-01-31 14:15:05 +0300

    2nd task in Python - Abstract Classes and Interfaces project

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -25,7 +25,7 @@ class Circle(Shape):
     """Circle shape"""
 
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         return math.pi * self.radius ** 2
