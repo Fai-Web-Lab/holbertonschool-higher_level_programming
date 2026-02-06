19a05267d925bc2d2f06ca5e7437430350274e37
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-06 19:01:11 +0300

    1st task in python-input/output project

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that contains a function to read a text file."""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
