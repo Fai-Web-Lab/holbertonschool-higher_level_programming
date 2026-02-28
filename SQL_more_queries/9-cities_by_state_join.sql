50f83ec2b7b595233ac0b146b6d71c9538f868d9
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-28 06:19:48 +0300

    10th task in more queries project

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all cities in hbtn_0d_usa with their respective state names
+-- Displays: cities.id - cities.name - states.name
+-- Sorted by cities.id in ascending order
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+JOIN states ON cities.state_id = states.id 
+ORDER BY cities.id ASC;
