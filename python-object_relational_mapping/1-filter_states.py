5963832bc5ce2759132e66881ac062d2577463f8
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-28 06:59:27 +0300

    2nd task in Python - Object-relational mapping project

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+
+    query = "SELECT * FROM states \
+             WHERE name LIKE BINARY 'N%' \
+             ORDER BY id ASC"
+    
+    cursor.execute(query)
+
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db.close()
