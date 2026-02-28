b65c27f6976ab1fb26fe3a16f525b14786a79b58
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-28 06:39:40 +0300

    2nd advanced task in more queries project

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- Uses a subquery to identify all Comedy shows and excludes them
+-- Results are sorted by show title in ascending order
+SELECT title
+FROM tv_shows
+WHERE title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
