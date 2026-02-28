f8a271febface9ff8063714af472b401dbd36550
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-28 06:38:14 +0300

    1st advanced task in more queries project

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Lists all genres not linked to the show Dexter
+-- Uses a subquery to find Dexter's genres and excludes them
+-- Results are sorted by genre name in ascending order
+SELECT name
+FROM tv_genres
+WHERE name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
