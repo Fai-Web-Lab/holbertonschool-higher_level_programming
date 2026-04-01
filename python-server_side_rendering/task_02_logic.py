f2393fc8c83c0e7a131f45ad220fffdc48f95c66
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-04-01 16:12:28 +0300

    Add Flask app with routes for home and items
    Implement Flask app with home and items routes.

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,23 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/items')
+def show_items():
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+            items_list = data.get("items", [])
+    except (FileNotFoundError, json.JSONDecodeError):
+        items_list = []
+        
+    return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+  
