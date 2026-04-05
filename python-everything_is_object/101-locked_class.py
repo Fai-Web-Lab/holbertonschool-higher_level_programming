851b7eb6ed5bbee20c3e499bee605f005ef6f4de
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-04-05 14:11:27 +0300

    Update 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ["first_name"]
+    """
+    A class that prevents dynamic attribute creation,
+    except for 'first_name'.
+    """
+    __slots__ = ['first_name']
